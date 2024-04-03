b8499a4859f37c617b7c5f72ec6376ff6b696a19
Qntlz <uzyke9517@gmail.com>
2024-04-03 12:15:40 +0800

    Added PrintMultiples.c

---
+++ FUNCTIONS/PrintMultiples.c
@@ -0,0 +1,27 @@
+/*
+    Create a function that takes in two integers A and B and prints 
+    out all the multiples of A between 1 and B
+*/
+
+#include <stdio.h>
+
+void printMultiples(int,int);
+
+int main()
+{
+    int firstNum, secNum;
+    printf("Enter first number: ");
+    scanf("%d", &firstNum);
+    printf("Enter second number: ");
+    scanf("%d", &secNum);
+    printMultiples(firstNum,secNum);
+    return 0;
+}
+
+void printMultiples(int firstNum,int secNum)
+{
+    for(int i = firstNum; i <= secNum; i++)
+    {
+        if(i % firstNum == 0) printf("%d\n",i);
+    }
+}
