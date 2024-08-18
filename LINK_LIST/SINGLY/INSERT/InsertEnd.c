57b44423c5ac02917ca82573dd96436b931428dd
Qntlz <uzyke9517@gmail.com>
2024-08-18 10:31:05 +0800

    Changed File Path

---
+++ LINK_LIST/SINGLY/INSERT/InsertEnd.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Insert at the END using Pointer to Pointer to Node (PPTN) Method
+
+// Define Node Structure
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node, *List;
+
+List makeNode(int);
+void populate(List *, List *);
+void display(List);
+void insertEnd(List *, int);
+void insertEndLast(List, int);      // Inserting End using last pointer
+
+int main()
+{
+    int num;
+    List head = NULL, last = NULL;
+
+    populate(&head, &last);
+    printf("Before Insert: ");
+    display(head);
+
+    // Demo for inserting end
+    printf("Enter number to insert: ");
+    scanf("%d", &num);
+
+    insertEnd(&head,num);         // Insert at the end using traversal
+    insertEndLast(last, num);            // Insert at the end using the last pointer
+
+    // Display inserted element
+    printf("After Insert: ");
+    display(head);
+
+    return 0;
+}
+
+List makeNode(int num)
+{
+    List newNode = (List)malloc(sizeof(Node)); // Dynamically Allocate Memory for Node
+
+    if (newNode == NULL)
+    {
+        printf("Memory Allocation Failed"); // Error Checking
+    }
+
+    newNode->data = num;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void populate(List *head, List *last)
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        List newNode = makeNode(i);
+        if (*head == NULL) // If the list is empty
+        {
+            *head = newNode;
+            *last = newNode;
+        }
+
+        (*last)->next = newNode;
+        *last = newNode;
+    }
+}
+
+void display(List head)
+{
+    for (; head != NULL; head = head->next) printf("%d ",head->data);
+    printf("\n");
+}
+
+void insertEnd(List *head, int target)
+{
+    List *trav;
+
+    // Traverse the list using PPTN Method
+    for (trav = head; *trav != NULL && (*trav)->data != target; trav = &(*trav)->next){}
+
+    // If the element is present exit
+    if (*trav != NULL)
+    {
+        printf("Element Already existed!");
+        return;
+    }
+
+    // If the element was not found proceed inserting at the last
+    *trav = makeNode(target);
+}
+
+void insertEndLast(List last, int target)
+{
+    last->next = makeNode(target);
+}
